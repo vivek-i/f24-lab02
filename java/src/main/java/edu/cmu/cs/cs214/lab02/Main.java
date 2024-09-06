87786918605a011462d6cbf885138717e087a713
vivek-i <vivek.isuk@gmail.com>
2024-09-06 02:43:22 -0400

    fix java encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
-
         Renderer renderer = new Renderer(rectangle);
-
         renderer.draw();
+
+        Square square = new Square(5);
+        Renderer squareRenderer = new Renderer(square);
+        squareRenderer.draw();
+
+        Circle circle = new Circle(5);
+        Renderer circleRenderer = new Renderer(circle);
+        circleRenderer.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,8 +1,8 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private final double height;
+    private final double width;
     
     public Rectangle(double height, double width){
         this.height = height;
